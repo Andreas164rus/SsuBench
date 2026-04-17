5df2113da9506b77389ed95d0fc4c3a5dea47977
Andrei Subbotkin <andreas164@yandex.ru>
2026-04-17 12:35:10 +0400

    register users

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,6 +5,8 @@ class Settings(BaseSettings):
     app_title: str = 'База'
     DATABASE_URL: str
     secret: str = 'SECRET' # this's for user's password
+    first_name_admin: str
+    last_name_admin: str
 
     class Config:
         env_file = '.env'
